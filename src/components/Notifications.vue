008624c498e6bf39b5efbec30f76c125d1830d2a
Karl Koch <hi@kejk.co>
2020-02-12 15:04:02 +0000

    Fixing styling on side nav items

--- src/components/Notifications.vue
+++ src/components/Notifications.vue
@@ -1,40 +1,30 @@
 <template>
-  <v-container class="py-0">
-    <v-row>
-      <v-col cols="9">
-      <v-card-text class="body-2 py-0">
-      {{ notification.date }}
-      </v-card-text>
-      <v-card-text class="body-1 font-weight-bold">
-      {{ notification.description }}
-      </v-card-text>
-      <v-card-text class="body-2 py-0 mb-4">
-      {{ notification.details }}
-      </v-card-text>
+  <v-list dense nav class="py-0">
+      <v-list-item link :to="notification.url">
+      <v-col cols="10">
+        <v-list-item-subtitle class="body-1 py-4">{{ notification.date }}</v-list-item-subtitle>
+        <v-list-item-title class="title font-weight-bold py-2">{{ notification.description }}</v-list-item-title>
+        <v-list-item-subtitle class="body-1 py-4">{{ notification.details }}</v-list-item-subtitle>
       </v-col>
-      <v-col cols="3" align-items="center" class="my-auto">
-      <div class="text-center">
-        <v-btn text icon :to="notification.url">
-            <v-icon
-              large
-            >
-              mdi-chevron-right
-            </v-icon>
-        </v-btn>
-      </div>
+      <v-col cols="2" align-items="center" class="my-auto">
+        <div class="text-center">
+          <v-btn text icon :to="notification.url">
+            <v-icon large>mdi-chevron-right</v-icon>
+          </v-btn>
+        </div>
       </v-col>
-    </v-row>
+      </v-list-item>
       <v-divider></v-divider>
-  </v-container>
+  </v-list>
 </template>
 
 <script>
 export default {
   name: "Notifications",
-  props: {    
-        notification: {
-            type: String
-        },
-    },
+  props: {
+    notification: {
+      type: String
+    }
+  }
 };
 </script>
