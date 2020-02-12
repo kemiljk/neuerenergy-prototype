21a359f20eb8c37005f46f8710b20b8bccaaf915
Karl Koch <hi@kejk.co>
2020-02-12 13:47:37 +0000

    Made notification component

---
+++ src/components/NotificationCards.vue
@@ -0,0 +1,42 @@
+<template>
+    <v-card
+        >
+        <v-card-title 
+        class="font-weight-bold grey lighten-3">
+        {{ notificationCard.headline }}
+        </v-card-title>
+        <v-row>
+            <v-col cols="12" v-for="notification in notifications" :key="`${notification.id}`">
+                <Notifications  :notification="notification"/>
+            </v-col>
+        </v-row>
+    </v-card>
+</template>
+
+<script>
+import Notifications from "../components/Notifications"
+
+export default {
+    name: "NotificationCards",
+    components: {
+    Notifications
+    },
+    props: {    
+        notificationCard: {
+            type: String
+        },
+    },
+    data() {
+        return {
+        notifications: [
+            {
+            id: 1,
+            date: "12 Feb 2020",
+            description: "PWR PPA • Added External Legal Counsel as Reviewer ",
+            assignee: "Harry McCammond "
+            },
+            ]
+        }
+    }
+};
+</script>
---
+++ src/components/Notifications.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="10">
+      <v-card-text class="body-2 grey--text py-0">
+      {{ notification.date }}
+      </v-card-text>
+      <v-card-text class="body-1 font-weight-bold">
+      {{ notification.description }}
+      </v-card-text>
+      <v-card-text class="body-2 grey--text py-0">
+      {{ notification.assignee }}
+      </v-card-text>
+      </v-col>
+      <v-col cols="2" align-items="center" class="mx-auto my-auto">
+        <v-btn text icon>
+            <v-icon
+              large
+            >
+              mdi-chevron-right
+            </v-icon>
+          </v-btn>
+        </v-col>
+      <v-divider></v-divider>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Notifications",
+  props: {    
+        notification: {
+            type: String
+        },
+    },
+};
+</script>
