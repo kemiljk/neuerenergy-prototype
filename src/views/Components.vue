93f4f1b8552c2c4b051800d0015ee50dd8395ad3
Karl Koch <hi@kejk.co>
2020-02-14 10:25:42 +0000

    Managed to fix component states in components page

--- src/views/Components.vue
+++ src/views/Components.vue
@@ -12,30 +12,30 @@
           <v-col cols="12" md="6" v-for="card in cards" :key="`${card.id}`">
             <Cards :card="card" />
           </v-col>
-        </v-row>
+    </v-row>
       </v-col>
     </v-row>
     <v-row>
       <v-col cols="12">
         <h2>Notification Cards</h2>
         <v-row>
-          <v-col
-          cols="12"
-          md="6"
-          v-for="card in notificationCards"
-          :key="`${card.id}`"
-          >
-          <NotificationCards :card="card" />
-          </v-col>
-        </v-row>
+      <v-col
+        cols="12"
+        md="6"
+        v-for="notificationCard in notificationCards"
+        :key="`${notificationCard.id}`"
+      >
+        <NotificationCards :notificationCard="notificationCard" />
+      </v-col>
+    </v-row>
       </v-col>
     </v-row>
     <v-row>
       <v-col cols="12">
         <h2>Slider Cards</h2>
         <v-row>
-          <v-col cols="12" md="4" v-for="slider in sliders" :key="`${slider.id}`">
-            <SliderCards :slider="slider" />
+          <v-col cols="12" md="4" v-for="card in sliders" :key="`${card.id}`">
+            <SliderCards :card="card" />
           </v-col>
         </v-row>
       </v-col>
@@ -44,35 +44,31 @@
 </template>
 
 <script>
-import { uuid } from '../utils'
-import Cards from "../components/Cards"
-import NotificationCards from "../components/NotificationCards"
+import { uuid } from "../utils";
+import Cards from "../components/Cards";
+import NotificationCards from "../components/NotificationCards";
 import SliderCards from "../components/SliderCards";
 
 export default {
-  name: "Components",
+  name: "Dashboard",
   components: {
     Cards,
-    NotificationCards,
-    SliderCards
+    SliderCards,
+    NotificationCards
   },
-  Data() {
+  data() {
     return {
-    cards: [
+      cards: [
         {
           id: uuid(),
-          headline: "Headline goes here",
-          title: "1,000",
-          description: 'And a description'
+          headline: "Total MWh purchased",
+          title: "0"
         },
-      ], 
-    notificationCards: [
+      ],
+      notificationCards: [
         {
           id: uuid(),
-          date: "27 OCT 2019",
-          description: "CLR PPA • Initiated by Harry McCammond on 23rd Dec 2019",
-          details: "Awaiting contract initiation with CLR",
-          url: "/notifications/#"
+          headline: "In-Negotiation PPAs"
         },
       ],
       sliders: [
@@ -82,8 +78,8 @@ export default {
           title: "50%",
           description: "24,149.90 Tonnes CO₂ per year"
         }
-      ],    
-    }
+      ]
+    };
   }
 };
 </script>
