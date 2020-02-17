5f68ff32df8151e27ee3a0e855c774b6937f1223
Karl Koch <hi@kejk.co>
2020-02-17 16:21:16 +0000

    Tweaked the nav to utilise our primary colour on icons

--- src/App.vue
+++ src/App.vue
@@ -26,7 +26,7 @@
 
         <v-list-item v-for="item in items" :key="item.title" link :to="item.url">
           <v-list-item-icon>
-            <v-icon>{{ item.icon }}</v-icon>
+            <v-icon color="primary">{{ item.icon }}</v-icon>
           </v-list-item-icon>
 
           <v-list-item-content>
