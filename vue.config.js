c9f5d066db281d4eb8db16b8173d17b47efd8c18
Karl Koch <hi@kejk.co>
2020-06-10 19:16:23 +0100

    fix: dependencies

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,24 @@
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ]
+  ],
+
+  lintOnSave: true,
+  filenameHashing: true,
+
+  chainWebpack: config => {
+    config.optimization.delete('splitChunks')
+  },
+
+  runtimeCompiler: true,
+  productionSourceMap: false,
+
+  pwa: {
+    name: 'NeuerEnergy Prototype',
+    themeColor: '#1c475c',
+    msTileColor: '#1c475c',
+    workboxOptions: {
+      skipWaiting: true
+    }
+  }
 }
